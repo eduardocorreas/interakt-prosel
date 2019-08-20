524dd7931159b46c9f696d285ff7047a006db8e3
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2019-08-20 20:53:15 -0300

    SS09 - Ajuste de Layout

--- src/components/layout/HeaderContent/index.js
+++ src/components/layout/HeaderContent/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Layout } from 'antd';
 const { Header} = Layout;
 
-// import { Container } from './styles';
-
 const HeaderContentComponent = ({title}) =>{
   return (
-    <Header style={{ background: '#fff', padding: 0, paddingLeft: 20, 
-      borderBottom: 'solid', marginBottom:30, fontSize: 18}}>
+    <Header style={styleHeader}>
         <h1>{title}</h1>
     </Header>
   )
 };
+
+const styleHeader = {
+  background: '#fff', padding: 0, paddingLeft: 20, 
+  borderBottom: 'solid', marginBottom:30, fontSize: 18
+}
 
 export default HeaderContentComponent;
